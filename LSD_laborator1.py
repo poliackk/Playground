76f125cb7de147c35974ee7b87a8f5f960e22e5f
Stana Adelina Diana <stana.adelina.diana@gmail.com>
2022-09-28 09:04:21 +0300

    Update LSD_laborator1.py

--- LSD_laborator1.py
+++ LSD_laborator1.py
@@ -1,8 +1,31 @@
 
 def print_hi(name):
-    print(f'Hi, {name}')
-    
-#comment
+    print("Hi, "+name)
+
+
+def suma(a, b):
+    return a+b
+
+
+def min_numere(a, b):
+    if a > b:
+        return b
+    else:
+        return a
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Student')
+
+    a = 10
+    print(a)
+
+    b = 35.6
+    print(b)
+
+    c = 'student'
+    print(c)
+
+    print("suma="+str(suma(7, 5)))
+
+    print("min="+str(min_numere(6, 3)))
